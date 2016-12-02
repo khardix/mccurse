9595c7e269c742bdcedd3f1228de8bd0e8c9782a
Jan Staněk <jstanek@redhat.com>
2016-12-02 21:29:34 +0100

    Detect travis build and set record mode accordingly

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,6 +11,8 @@ CASSETE_DIR = 'tests/cassetes/'
 if not os.path.exists(CASSETE_DIR):
     os.makedirs(CASSETE_DIR)
 
+record_mode = 'none' if os.environ.get('TRAVIS_BUILD') else 'once'
+
 with betamax.Betamax.configure() as config:
     config.cassette_library_dir = CASSETE_DIR
-    config.default_cassette_options['record_mode'] = 'once'
+    config.default_cassette_options['record_mode'] = record_mode
