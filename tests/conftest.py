541408491c453be8f0f8812169777eae14125635
Jan "Khardix" Staněk <khardix@gmail.com>
2017-02-05 20:54:11 +0100

    Minimize shared fixtures dependencies on tested code

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,13 +2,13 @@
 
 
 import os
+from datetime import datetime, timezone
 from pathlib import Path
 
 import betamax
 import pytest
 
-from mccurse import addon, curse
-from mccurse.util import yaml
+from mccurse import addon, curse, proxy
 
 
 # Ensure cassete dir
@@ -47,45 +47,41 @@ def tinkers_construct() -> addon.Mod:
 
 
 @pytest.fixture
-def tinkers_construct_file() -> addon.File:
+def tinkers_construct_file(tinkers_construct) -> addon.File:
     """Tinkers construct file."""
 
-    yml = """\
-    !modfile
-    file:
-        date: 2016-12-07T18:35:45+00:00
-        dependencies: [74924]
-        id: 2353329
-        name: TConstruct-1.10.2-2.6.1.jar
-        release: Release
-        url: https://addons.cursecdn.com/files/2353/329/TConstruct-1.10.2-2.6.1.jar
-    id: 74072
-    name: Tinkers Construct
-    summary: Modify all the things, then do it again!
-    """
-
-    return yaml.load(yml)
+    return addon.File(
+        id=2353329,
+        mod=tinkers_construct,
+        name='TConstruct-1.10.2-2.6.1.jar',
+        date=datetime(
+            year=2016, month=12, day=7,
+            hours=18, minutes=35, seconds=45,
+            tz=timezone.utc,
+        ),
+        release=proxy.Release.Release,
+        url='https://addons.cursecdn.com/files/2353/329/TConstruct-1.10.2-2.6.1.jar',
+        dependencies=[74924],
+    )
 
 
 @pytest.fixture
 def mantle_file() -> addon.File:
     """Mantle (Tinkers dependency) file."""
 
-    yml = """\
-    !modfile
-    file:
-        date: 2017-01-09T19:40:41+00:00
-        dependencies: []
-        id: 2366244
-        name: Mantle-1.10.2-1.1.4.jar
-        release: Release
-        url: https://addons.cursecdn.com/files/2366/244/Mantle-1.10.2-1.1.4.jar
-    id: 74924
-    name: Mantle
-    summary: ''
-    """
-
-    return yaml.load(yml)
+    return addon.File(
+        id=2366244,
+        mod=addon.Mod(id=74924, name='Mantle', summary=''),
+        name='Mantle-1.10.2-1.1.4.jar',
+        date=datetime(
+            year=2017, month=1, day=9,
+            hours=19, minutes=40, seconds=41,
+            tz=timezone.utc,
+        ),
+        release=proxy.Release.Release,
+        url='https://addons.cursecdn.com/files/2366/244/Mantle-1.10.2-1.1.4.jar',
+        dependencies=[],
+    )
 
 
 @pytest.fixture
