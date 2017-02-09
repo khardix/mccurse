ee9bd7e7ca8847ed5bf453a789a6ed23abd46ee8
Jan "Khardix" Staněk <khardix@gmail.com>
2017-02-09 21:29:53 +0100

    Move available_file fixtures to shared file

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,7 @@ from pathlib import Path
 
 import betamax
 import pytest
+import responses
 
 from mccurse import addon, curse, proxy
 
@@ -113,3 +114,189 @@ def minecraft() -> curse.Game:
         'version': '1.10.2',
     }
     return curse.Game(**data)
+
+
+@pytest.fixture
+def available_files() -> dict:
+    """Test set of available files for Tinkers Construct.
+
+    Ordered by date ascending: 2349845[A], 2353329[R], 2366245[B]
+    """
+
+    jsn = {"files": [
+        {
+            "alternate_file_id": 0,
+            "file_name": "TConstruct-1.10.2-2.6.1.jar",
+            "is_available": True,
+            "dependencies": [
+                {
+                    "add_on_id": 74924,
+                    "type": "Required"
+                }
+            ],
+            "file_date": "2016-12-07T18:35:45",
+            "file_status": "SemiNormal",
+            "file_name_on_disk": "TConstruct-1.10.2-2.6.1.jar",
+            "id": 2353329,
+            "download_url":
+                "https://addons.cursecdn.com/files/2353/329/TConstruct-1.10.2-2.6.1.jar",
+            "package_fingerprint": 1768070072,
+            "is_alternate": False,
+            "release_type": "Release",
+            "game_version": [
+                "1.10.2"
+            ]
+        },
+        {
+            "alternate_file_id": 0,
+            "file_name": "TConstruct-1.10.2-2.6.2.jar",
+            "is_available": True,
+            "dependencies": [
+                {
+                    "add_on_id": 74924,
+                    "type": "Required"
+                }
+            ],
+            "file_date": "2017-01-09T19:41:50",
+            "file_status": "SemiNormal",
+            "file_name_on_disk": "TConstruct-1.10.2-2.6.2.jar",
+            "id": 2366245,
+            "download_url":
+                "https://addons.cursecdn.com/files/2366/245/TConstruct-1.10.2-2.6.2.jar",
+            "package_fingerprint": 1770865161,
+            "is_alternate": False,
+            "release_type": "Beta",
+            "game_version": [
+                "1.10.2"
+            ]
+        },
+        {
+            "alternate_file_id": 0,
+            "file_name": "TConstruct-1.10.2-2.6.0.jar",
+            "is_available": True,
+            "dependencies": [
+                {
+                    "add_on_id": 74924,
+                    "type": "Required"
+                }
+            ],
+            "file_date": "2016-11-27T16:25:15",
+            "file_status": "SemiNormal",
+            "file_name_on_disk": "TConstruct-1.10.2-2.6.0.jar",
+            "id": 2349845,
+            "download_url":
+                "https://addons.cursecdn.com/files/2349/845/TConstruct-1.10.2-2.6.0.jar",
+            "package_fingerprint": 1097160304,
+            "is_alternate": False,
+            "release_type": "Alpha",
+            "game_version": [
+                "1.10.2"
+            ]
+        },
+    ]}
+
+    return jsn
+
+
+@pytest.fixture
+def available_tinkers_tree(tinkers_construct, mantle_file) -> responses.RequestsMock:
+    """JSON for tree resolution."""
+
+    pool = {
+        proxy.HOME_URL + '/addon/{tinkers_construct.id}/files'.format_map(locals()): {
+            'files': [
+                {
+                    "release_type": "Release",
+                    "file_status": "SemiNormal",
+                    "game_version": [
+                        "1.10.2"
+                    ],
+                    "file_name_on_disk": "TConstruct-1.10.2-2.6.1.jar",
+                    "file_date": "2016-12-07T18:35:45",
+                    "download_url":
+                        "https://addons.cursecdn.com/files/2353/329/TConstruct-1.10.2-2.6.1.jar",
+                    "alternate_file_id": 0,
+                    "id": 2353329,
+                    "package_fingerprint": 1768070072,
+                    "is_available": True,
+                    "file_name": "TConstruct-1.10.2-2.6.1.jar",
+                    "is_alternate": False,
+                    "dependencies": [
+                        {
+                            "type": "Required",
+                            "add_on_id": 74924
+                        }
+                    ]
+                },
+                {
+                    "release_type": "Beta",
+                    "file_status": "SemiNormal",
+                    "game_version": [
+                        "1.10.2"
+                    ],
+                    "file_name_on_disk": "TConstruct-1.10.2-2.6.2.jar",
+                    "file_date": "2017-01-09T19:41:50",
+                    "download_url":
+                        "https://addons.cursecdn.com/files/2366/245/TConstruct-1.10.2-2.6.2.jar",
+                    "alternate_file_id": 0,
+                    "id": 2366245,
+                    "package_fingerprint": 1770865161,
+                    "is_available": True,
+                    "file_name": "TConstruct-1.10.2-2.6.2.jar",
+                    "is_alternate": False,
+                    "dependencies": [
+                        {
+                            "type": "Required",
+                            "add_on_id": 74924
+                        }
+                    ]
+                },
+            ]
+        },
+        proxy.HOME_URL + '/addon/{mantle_file.mod.id}/files'.format_map(locals()): {
+            'files': [
+                {
+                    "release_type": "Release",
+                    "file_status": "SemiNormal",
+                    "game_version": [
+                        "1.10.2"
+                    ],
+                    "file_name_on_disk": "Mantle-1.10.2-1.1.4.jar",
+                    "file_date": "2017-01-09T19:40:41",
+                    "download_url":
+                        "https://addons.cursecdn.com/files/2366/244/Mantle-1.10.2-1.1.4.jar",
+                    "alternate_file_id": 0,
+                    "id": 2366244,
+                    "package_fingerprint": 4219802267,
+                    "is_available": True,
+                    "file_name": "Mantle-1.10.2-1.1.4.jar",
+                    "is_alternate": False,
+                    "dependencies": []
+                },
+                {
+                    "release_type": "Beta",
+                    "file_status": "SemiNormal",
+                    "game_version": [
+                        "1.9"
+                    ],
+                    "file_name_on_disk": "Mantle-1.9-0.10.1.jar",
+                    "file_date": "2016-05-26T15:37:09",
+                    "download_url":
+                        "https://addons.cursecdn.com/files/2302/982/Mantle-1.9-0.10.1.jar",
+                    "alternate_file_id": 0,
+                    "id": 2302982,
+                    "package_fingerprint": 3396617729,
+                    "is_available": True,
+                    "file_name": "Mantle-1.9-0.10.1.jar",
+                    "is_alternate": False,
+                    "dependencies": []
+                },
+            ]
+        },
+    }
+
+    requests_mock = responses.RequestsMock()
+    for url, jsn in pool.items():
+        requests_mock.add(responses.GET, url, json=jsn)
+
+    return requests_mock
